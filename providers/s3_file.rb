573dc2c8c70d05aefacbe6151e7180cc722b1c8d
Tom Howe <turtlebender@gmail.com>
2013-04-30 16:21:38 -0500

    adding support for owner/group

--- providers/s3_file.rb
+++ providers/s3_file.rb
@@ -1,18 +1,32 @@
 # Download a file from s3 and store it locally
 # Requires a bucket, key and local path in which to store the file.
 action :download do
-  ruby_block "download s3://#{@new_resource.bucket}/#{@new_resource.key}" do
-    block do
-      File.open(@new_resource.local_file, 'w') do |file|
-        obj.read do |chunk|
-          file.write(chunk)
-        end
-      end
+  unless exists?
+    s3_download_file = Tempfile.new('s3')
+    obj.read do |chunk|
+      s3_download_file.write(chunk)
     end
-    not_if "test -f #{@new_resource.local_file}"
+    s3_download_file.rewind()
+    move_and_clean_up s3_download_file
+    new_resource.updated_by_last_action(true)
   end
 end
 
-action :upload do
-  log 'Upload doesn\'t currently do anything'
+private
+
+def exists?
+  ::File.exists?(new_resource.path)
+end
+
+def move_and_cleanup(s3_download_file)
+  Chef::Log.info("#{@new_resource} download completed")
+  f = file @new_resource.path do
+    content s3_download_file.read()
+    backup false
+    owner new_resource.owner
+    group new_resource.group
+  end
+  f.run_action(:create)
+  s3_download_file.close
+  s3_download_file.unlink    # deletes the temp file
 end
--- resources/s3_file.rb
+++ resources/s3_file.rb
@@ -1,7 +1,9 @@
-actions :download, :upload
+actions :download
 
 attribute :bucket, :type => String, :required => true
 attribute :key, :type => String, :required => true
-attribute :local_file, :type => String, :required => true, :name_attribute => true
+attribute :path, :type => String, :required => true, :name_attribute => true
+attribute :owner, :type => String
+attribute :group, :type => String
 
 default_action :download
