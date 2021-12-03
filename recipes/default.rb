ec7fab9352c5592e40911d617027cf24b1ec6832
Tom Howe <turtlebender@gmail.com>
2021-12-03 09:41:32 -0600

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,3 @@
-#
-# Cookbook Name:: aws_sdk
-# Recipe:: default
-#
-# Copyright 2013, NarrativeScience, Inc
-#
-# All rights reserved - Do Not Redistribute
-#
-
 aws_sdk_connection 'base' do
   action :install
 end
